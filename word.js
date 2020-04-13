3eaf7157c4f6b9bc6a7acba059a5630e7f787ccc
Bailee Dastugue <bailee.dast@gmail.com>
2020-04-13 14:45:26 -0500

    deleted unnecessary variables and added comments to word.js

--- word.js
+++ word.js
@@ -1,18 +1,21 @@
 var Letter = require("./letter");
-var randomWord = "";
 var wordRep = "";
 
+// a constructor
 function Word () {
+    // empty array
     this.letterArray = [];
-    this.wordArray = [];
     this.createArray = function(randomWord) {
+        // stores the letters of the word separately in letterArray
         this.letterArray = randomWord.split("");
-        // creates an array of letters using the constructor 
+
+        // creates an array of letters using the Letter constructor
         for (var i = 0; i < this.letterArray.length; i++){
-            this.wordArray.push(this.letterArray[i]);
             this.letterArray[i] = new Letter(this.letterArray[i]);
         }
     }
+    // check to see if the letter is in the word
+    // consoles a string of underscores or letters that updates
     this.callCheck = function (guess) {
         for (var i = 0; i < this.letterArray.length; i++) {
             this.letterArray[i].checkGuess(guess);
@@ -21,7 +24,5 @@ function Word () {
         console.log(wordRep);
     }
 }
-
-
 
 module.exports = Word;
